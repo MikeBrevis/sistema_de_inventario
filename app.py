d6b4a1fc5f991486706e776b4691259089ea53cf
MikeBrevis <michel.brevis@gmail.com>
2026-01-22 16:12:28 -0300

    Update DB

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///inventario.db'
+db = SQLAlchemy(app)
 
 @app.route('/')
 def index():
@@ -8,5 +12,6 @@ def index():
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
 
