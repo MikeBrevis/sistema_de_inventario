90c2a5463daef97709f73880f4a662f60a3ec8a6
MikeBrevis <michel.brevis@gmail.com>
2026-02-05 12:22:04 -0300

    Datos clientes

---
+++ insercion_datos.sql
@@ -0,0 +1,16 @@
+INSERT INTO empleado (
+    id_empleado, nombre, telefono, correo, fecha_de_creacion, fecha_de_actualizacion
+)
+VALUES 
+(2, 'Juan Pérez', '912345678', 'juan.perez@empresa.com', DEFAULT, '2026-02-05'),
+(3, 'Ana Martínez', '922334455', 'ana.mtz@empresa.com', DEFAULT, DEFAULT),
+(4, 'Carlos Ruiz', '933445566', 'carlos.ruiz@empresa.com', DEFAULT, DEFAULT),
+(5, 'Elena Gómez', '944556677', 'elena.gomez@empresa.com', DEFAULT, DEFAULT),
+(6, 'Ricardo Soto', '955667788', 'rsoto@empresa.com', DEFAULT, DEFAULT),
+(7, 'Lucía Fernández', '966778899', 'lucia.f@empresa.com', DEFAULT, DEFAULT),
+(8, 'Diego Morales', '977889900', 'diego.morales@empresa.com', DEFAULT, DEFAULT),
+(9, 'Sofía Castro', '988990011', 'scastro@empresa.com', DEFAULT, DEFAULT),
+(10, 'Andrés Vera', '999001122', 'avera@empresa.com', DEFAULT, DEFAULT);
+
+select * from empleado
+
